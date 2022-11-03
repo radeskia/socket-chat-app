1a2f100875423093ae0459465215a1052169ad05
Aleksandar Radeski <65304039+radeskia@users.noreply.github.com>
2022-11-03 11:20:17 +0100

    Refactored single message into separate component & added types for props

---
+++ client/src/components/ChatApp/ChatMessage.tsx
@@ -0,0 +1,62 @@
+import { format } from "date-fns";
+import { MessageProps } from "../../interfaces/ChatMessageProps";
+import { useAuth } from "../../providers/auth-context";
+
+const ChatMessage = ({ message, avatar, setModal }: MessageProps) => {
+    const { currentUser } = useAuth();
+
+    return (
+        <div className="flex justify-between" key={message._id ?? message.time}>
+            <p
+                className={`flex text-gray-500 p-1 my-2 px-2 items-end ${
+                    currentUser === message.sender ? "order-1" : "order-2"
+                }`}
+            >
+                {format(new Date(+message.time), "HH:mm")}
+            </p>
+            <div
+                className={`flex ${
+                    currentUser === message.sender ? "order-2" : "order-1"
+                }`}
+            >
+                <div
+                    className={`relative text-blue-500 p-1 bg-gray-800 my-1 rounded-md px-2 break-all text-justify flex items-center ${
+                        currentUser === message.sender
+                            ? "order-1 pl-7"
+                            : "order-2 pr-7"
+                    }`}
+                >
+                    <p>{message.message}</p>
+                    <div
+                        className={`absolute h-3 w-3 border-2 border-gray-600 rounded-full bottom-1 ${
+                            currentUser === message.sender
+                                ? "left-1"
+                                : "right-1"
+                        } ${message.seen ? "bg-gray-600" : ""}`}
+                    ></div>
+                </div>
+                <div
+                    className={`block my-auto mx-2 w-8 h-8 shrink-0 cursor-pointer ${
+                        currentUser === message.sender ? "order-2" : "order-1"
+                    }`}
+                    onClick={() =>
+                        setModal({
+                            show: true,
+                            email: message.sender,
+                            avatar: avatar,
+                            messageCount: 0,
+                        })
+                    }
+                >
+                    <img
+                        src={avatar}
+                        alt="User Avatar"
+                        className="w-full h-full rounded-full object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ChatMessage;
--- client/src/components/ChatApp/ChatMessages.tsx
+++ client/src/components/ChatApp/ChatMessages.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useMemo } from "react";
 import { format } from "date-fns";
 import { useAuth } from "../../providers/auth-context";
 import ChatUsers from "./ChatUsers";
+import ChatMessage from "./ChatMessage";
 
 const ChatMessages = ({
     messages,
@@ -55,118 +56,25 @@ const ChatMessages = ({
                                     {reversed.length &&
                                         reversed.map((message: any) => {
                                             return (
-                                                <div
-                                                    className="flex justify-between"
+                                                <ChatMessage
+                                                    message={message}
+                                                    avatar={
+                                                        avatarsData.data.find(
+                                                            (user: any) => {
+                                                                return (
+                                                                    user.email ===
+                                                                    message.sender
+                                                                );
+                                                            }
+                                                        ).avatar ??
+                                                        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                                                    }
+                                                    setModal={setModal}
                                                     key={
                                                         message._id ??
                                                         message.time
                                                     }
-                                                >
-                                                    <p
-                                                        className={`flex text-gray-500 p-1 my-2 px-2 items-end ${
-                                                            currentUser ===
-                                                            message.sender
-                                                                ? "order-1"
-                                                                : "order-2"
-                                                        }`}
-                                                    >
-                                                        {format(
-                                                            new Date(
-                                                                +message.time
-                                                            ),
-                                                            "HH:mm"
-                                                        )}
-                                                    </p>
-                                                    <div
-                                                        className={`flex ${
-                                                            currentUser ===
-                                                            message.sender
-                                                                ? "order-2"
-                                                                : "order-1"
-                                                        }`}
-                                                    >
-                                                        <div
-                                                            className={`relative text-blue-500 p-1 bg-gray-800 my-1 rounded-md px-2 break-all text-justify flex items-center ${
-                                                                currentUser ===
-                                                                message.sender
-                                                                    ? "order-1 pl-7"
-                                                                    : "order-2 pr-7"
-                                                            }`}
-                                                        >
-                                                            <p>
-                                                                {
-                                                                    message.message
-                                                                }
-                                                            </p>
-                                                            <div
-                                                                className={`absolute h-3 w-3 border-2 border-gray-600 rounded-full bottom-1 ${
-                                                                    currentUser ===
-                                                                    message.sender
-                                                                        ? "left-1"
-                                                                        : "right-1"
-                                                                } ${
-                                                                    message.seen
-                                                                        ? "bg-gray-600"
-                                                                        : ""
-                                                                }`}
-                                                            ></div>
-                                                        </div>
-                                                        <div
-                                                            className={`block my-auto mx-2 w-8 h-8 shrink-0 cursor-pointer ${
-                                                                currentUser ===
-                                                                message.sender
-                                                                    ? "order-2"
-                                                                    : "order-1"
-                                                            }`}
-                                                            onClick={() =>
-                                                                setModal({
-                                                                    show: true,
-                                                                    email: message.sender,
-                                                                    avatar: avatarsData.data.find(
-                                                                        (
-                                                                            user: any
-                                                                        ) => {
-                                                                            return (
-                                                                                user.email ===
-                                                                                message.sender
-                                                                            );
-                                                                        }
-                                                                    ).avatar,
-                                                                    messageCount:
-                                                                        messages.filter(
-                                                                            (
-                                                                                msg: any
-                                                                            ) => {
-                                                                                return (
-                                                                                    msg.sender ===
-                                                                                    message.sender
-                                                                                );
-                                                                            }
-                                                                        )
-                                                                            .length,
-                                                                })
-                                                            }
-                                                        >
-                                                            <img
-                                                                src={`${
-                                                                    avatarsData.data.find(
-                                                                        (
-                                                                            user: any
-                                                                        ) => {
-                                                                            return (
-                                                                                user.email ===
-                                                                                message.sender
-                                                                            );
-                                                                        }
-                                                                    ).avatar ??
-                                                                    "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
-                                                                }`}
-                                                                alt=""
-                                                                className="w-full h-full rounded-full object-cover"
-                                                            />
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                                />
                                             );
                                         })}
                                 </div>
