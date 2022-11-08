d72ec2258ce7551f718bb10b94a2f557ecbdee5c
Aleksandar Radeski <65304039+radeskia@users.noreply.github.com>
2022-11-08 12:57:14 +0100

    Minor merge on user controller and auth controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,7 +10,6 @@ const { validate } = require("../middleware/validate");
 const {
     validate_refresh_token,
 } = require("../middleware/validate_refresh_token");
-const auth_controller = require("../controllers/auth_controller");
 
 router
     //Test routes
@@ -29,7 +28,7 @@ router
     //User routes
     .post("/register", validate(REGISTER_SCHEMA), user_controller.register)
     .post("/login", validate(LOGIN_SCHEMA), user_controller.login)
-    .post("/refresh", validate_refresh_token, auth_controller.refresh)
+    .post("/refresh", validate_refresh_token, user_controller.refresh)
 
     // Chats routes
     .get("/users/", user_controller.get_users)
